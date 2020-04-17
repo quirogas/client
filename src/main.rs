bc1138480678f620b5e836c9b0e9f786f6248f24
Santiago Quiroga <quirogas@mail.gvsu.edu>
2020-04-17 15:54:17 -0400

    something is something eh ?

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use hyper::{Body, Client, Request };
+use hyper::{Body, Client, Request, Response  };
+use hyper_tls::HttpsConnector;
+use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::new();
+    let request = Request::get("https://httpbin.org").body(Body::from(""))?;
 
-    let request = Request::get("http://httpbin.org").body(Body::from(""))?;
+    let response = send(request).await?;
 
-    let response = client.request(request).await?;
-    println!("{:?}", response);
+    println!("after send: {:?}", response);
 
     Ok(())
+}
+
+async fn send(request: Request<Body>) -> Result<Response<Body>,Box<dyn Error>> {
+ let client = Client::builder().build::<>(HttpsConnector::new());
+ let response = client.request(request).await?;
+    
+ match  response.status() {
+     hyper::StatusCode::OK => Ok(response),
+     _ => Err("there was an error with the request".into())
+
+ }
 }

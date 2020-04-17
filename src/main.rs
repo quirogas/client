7025952f1704addb13d87c47dbce1050c4181e11
Santiago Quiroga <quirogas@mail.gvsu.edu>
2020-04-17 16:55:15 -0400

    reqwest get post and auth

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,16 @@
-use hyper::{Body, Client, Request, Response  };
-use hyper_tls::HttpsConnector;
-use std::error::Error;
+use reqwest::{Client};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let request = Request::get("https://httpbin.org").body(Body::from(""))?;
 
-    let response = send(request).await?;
 
-    println!("after send: {:?}", response);
+let client = Client::new();
+let response_get = client.get("https://httpbin.org/get").send();
+let response_post = client.post("http://httpbin.org/post").send();
+
+let response_get_auth = client.get("https://httpbin.org/basic-auth/root/toor").basic_auth("root", Some("toor")).send();
+
+    println!(" GET: {:?}\n\nPOST: {:?}\n\nAUTH: {:?}",response_get.await, response_post.await, response_get_auth.await);
 
     Ok(())
-}
-
-async fn send(request: Request<Body>) -> Result<Response<Body>,Box<dyn Error>> {
- let client = Client::builder().build::<>(HttpsConnector::new());
- let response = client.request(request).await?;
-    
- match  response.status() {
-     hyper::StatusCode::OK => Ok(response),
-     _ => Err("there was an error with the request".into())
-
- }
 }
